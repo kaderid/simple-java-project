511565078a2e3e4309b9ec0bfbf2db4903d4aa56
Abdoulkader Boubacar IDDE <abdoulkader.bi@kuikops.com>
2025-08-27 12:50:04 +0200

    fix

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins and GitHub kader: kadri!");
+        System.out.println("Hello, Jenkins and GitHub kader: kadri 2.0!");
     }
 }
