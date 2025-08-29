3484175a19a4cce14f37464163ddb9552e42e7ac
Abdoulkader Boubacar IDDE <abdoulkader.bi@kuikops.com>
2025-08-29 16:54:06 +0200

    Another commit to test multibranch

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins and GitHub kader: kadri 2.0!");
+        System.out.println("Hello, Jenkins and GitHub kader: Multibranch:");
     }
 }
+
